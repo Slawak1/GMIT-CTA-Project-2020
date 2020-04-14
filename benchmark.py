e2ed1b9521bf11dc96dbc4c116fcfa1072876340
Slawak1 <sowa.slawomir@gmail.com>
2020-04-14 23:35:24 +0100

    Main loop added

--- benchmark.py
+++ benchmark.py
@@ -6,6 +6,8 @@
 
 from random import randint
 import time
+import numpy as np
+
 
 def main():
     pass
@@ -19,15 +21,16 @@ def generate_array(n):
     return array
 
 def bubble_sort(arr): 
+
     start_time = time.time()
     n = len(arr) 
-  
+
     # Traverse through all array elements 
     for i in range(n): 
-  
-        # Last i elements are already in place 
+
+    # Last i elements are already in place 
         for j in range(0, n-i-1): 
-  
+
             # traverse the array from 0 to n-i-1 
             # Swap if the element found is greater 
             # than the next element 
@@ -37,13 +40,23 @@ def bubble_sort(arr):
     time_elapsed = end_time - start_time
     return time_elapsed
 
+# def calc_avg(func, input_size):
+#     for i in range(10):
+#         bubble_sort
+
 def main():
 
     input_size = [250,500,750,1000,1250,2500]
     output_arr = []
     for i in input_size:
-        out = bubble_sort(generate_array(i))
-        output_arr.append(out)
+        some_list = []
+        for j in range(10):
+            
+            out = bubble_sort(generate_array(i))
+            some_list.append(out)
+            
+        print(some_list, np.mean(some_list))     
+        output_arr.append(np.mean(some_list))
     
     print(output_arr)
     
