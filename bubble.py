cce377d583bafa13181f55ae1006683e4d86010d
slawak1 <sowa.slawomir@gmail.com>
2020-04-23 16:18:00 +0000

    comments added

--- bubble.py
+++ bubble.py
@@ -15,20 +15,19 @@ def bubble_sort(arr):
             arr: list
                 returns sorted array
     '''
-
+    # length of the array assigned to variable 
     n = len(arr) 
 
-    # Traverse through all array elements 
+    # Loop over all array elements 
     for i in range(n): 
 
-    # Last i elements are already in place 
+    # After each iteration the largest element is placed at the end
         for j in range(0, n-i-1): 
 
-            # traverse the array from 0 to n-i-1 
-            # Swap if the element found is greater 
-            # than the next element 
+            # Swap if the element arr[j]is greater than arr[j+1]
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
-
+    
+    # return sorted array
     return arr
 
